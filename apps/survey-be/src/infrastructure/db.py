d5e03152b937be7dfcb49ae9163283f9a5b40b97
Lua <lua.dev26@gmail.com>
2025-07-16 22:55:44 -0500

    chore: add dotenv

--- apps/survey-be/src/infrastructure/db.py
+++ apps/survey-be/src/infrastructure/db.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 
+load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
