d4a8e31ea033fbb1c252af2a42e168223cc29389
Lua <lua.dev26@gmail.com>
2025-07-17 01:30:11 -0500

    chore: validate database url

--- apps/survey-be/src/infrastructure/db.py
+++ apps/survey-be/src/infrastructure/db.py
@@ -7,8 +7,12 @@ from sqlalchemy.orm import sessionmaker, Session
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL environment variable not set")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 def get_db():
     db: Session = SessionLocal()
